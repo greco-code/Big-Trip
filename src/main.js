a65064747692ec0d315651dc81bd596d4bc9c0b4
George <naifonovgk@gmail.com>
2021-03-30 23:56:50 +0300

    Feat: trip-sort component added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import {createSiteMenuTemplate} from './createSiteMenuTemplate';
 import {createTripInfoTemplate} from './createTripInfoTemplate';
 import {createTripFilterTemplate} from './createTripFilterTemplate';
+import {createTripSortTemplate} from './createTripSortTemplate';
 
 const headerMain = document.querySelector('.trip-main');
+const pageMain = document.querySelector('.page-main');
 const headerMenuContainer = headerMain.querySelector('.trip-controls__navigation');
 const tripFilterContainer = headerMain.querySelector('.trip-controls__filters');
+const tripEventsContainer = pageMain.querySelector('.trip-events');
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -13,3 +16,5 @@ const render = (container, template, place) => {
 render(headerMenuContainer, createSiteMenuTemplate(), 'beforeend');
 render(headerMain, createTripInfoTemplate(), 'afterbegin');
 render(tripFilterContainer, createTripFilterTemplate(), 'afterbegin');
+render(tripEventsContainer, createTripSortTemplate(), 'afterbegin');
+
