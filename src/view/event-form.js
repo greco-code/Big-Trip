de282cfbdcb132941fc0b6fd729352f359a6ed65
george <naifonovgk@gmail.com>
2021-05-11 07:18:20 +0300

    Feat: form validation added

--- src/view/event-form.js
+++ src/view/event-form.js
@@ -8,6 +8,7 @@ import flatpickr from 'flatpickr';
 import '../../node_modules/flatpickr/dist/flatpickr.min.css';
 import dayjs from 'dayjs';
 import {nanoid} from 'nanoid';
+import he from 'he';
 
 const BLANK_EVENT = {
   base_price: '',
@@ -153,7 +154,12 @@ const createPointForm = (event) => {
                 <label class="event__label  event__type-output" for="event-destination-1">
                   ${type}
                 </label>
-                <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination.name}" list="destination-list-1">
+                <input class="event__input  event__input--destination"
+                 id="event-destination-1"
+                 type="text"
+                 name="event-destination"
+                 value="${he.encode(destination.name)}"
+                 list="destination-list-1" required>
                 <datalist id="destination-list-1">
                   ${dataList}
                 </datalist>
@@ -172,7 +178,7 @@ const createPointForm = (event) => {
                   <span class="visually-hidden">Price</span>
                   &euro;
                 </label>
-                <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${base_price}">
+                <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${base_price}" required>
               </div>
 
               <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
